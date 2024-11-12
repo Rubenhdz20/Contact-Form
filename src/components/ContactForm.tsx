46a5ad36a2c0f134238430404961e526322fc453
Rubenhdz20 <rubhernandez42@gmail.com>
2024-11-11 19:09:26 -0600

    reestructure files and folders

---
+++ src/components/ContactForm.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { useForm, SubmitHandler } from 'react-hook-form';
+
+function ContactForm () {
+
+    return(
+        <form>
+            <h1>Contact Us</h1>
+        </form>
+    )
+};
+
+export default ContactForm;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,38 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
-    extend: {},
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px',
+    },
+    colors: {
+      'strong-grey': '#2A4144',
+      'light-grey': '#86A2A5',
+      'white': '#FFFFFF',
+      'strong-green': '#0C7D69',
+      'light-green': '#E0F1E8',
+      'errors': '#D73C3C',
+    },
+    fontFamily: {
+      serif: ["Karla", 'sans-serif'],
+    },
+    extend: {
+      spacing: {
+        '100': '.5rem',
+        '150': '.75rem',
+        '200': '1rem',
+        '300': '1.5rem',
+        '400': '2rem',
+        '500': '2.5rem',
+        '500': '8rem',
+      },
+    },
   },
   plugins: [],
 }
